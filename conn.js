98f84a66e99fe97ff602275d75cbde10ddf092d1
pavansawant777 <126850815+pavansawant777@users.noreply.github.com>
2024-12-04 13:04:13 +0530

    Update conn.js

--- conn.js
+++ conn.js
@@ -2,10 +2,10 @@ var mysql = require('mysql');
 var util = require('util');
 
 var conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root', 
-    password: '',
-    database: 'mace_project_db',
+    host: 'bxvmy8bbbkdmgjagiafe-mysql.services.clever-cloud.com',
+    user: 'udxjvmeehjinkm13', 
+    password: 'TMfbXxgVa36alb8qVuSU',
+    database: 'bxvmy8bbbkdmgjagiafe',
 });
 
 var exe = util.promisify(conn.query).bind(conn);
